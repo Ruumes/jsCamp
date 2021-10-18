e049cbc577b8814cb7fd67d65b2301b32d8951e7
Asus <Asus@DESKTOP-QNL9LTL>
2021-10-19 00:02:05 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,29 @@
+console.log("merhaba kodlama.io")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+{
+    let dolarDun = 9.10
+}
+ console.log(dolarDun)
+
+ const euroDun = 11.2
+
+ console.log(euroDun)
+
+ let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+
+console.log("<ul>")
+for(let i = 0;i<3;i++){
+    console.log("<li>"+konutKredileri[i]+"</li")
+}
+console.log("</ul>")
+//  for {
+//     <ul>
+//         <li>Kamu Konut Kredisi</li>
+//         <li>Emlak Konut Kredisi</li>
+//         <li>Kamu Konut Kredisi</li>
+//     </ul>
+//  }
+console.log(konutKredileri)
